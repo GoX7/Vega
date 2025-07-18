10ff3f900d18c5aaff83684b3e52051f28d4f5b9
Ilya <maslennikovila82@gmail.com>
2025-07-18 16:20:13 +0300

    Update examples :)

---
+++ examples/bind-basic.go
@@ -0,0 +1,42 @@
+package examples
+
+import (
+	"fmt"
+
+	"github.com/gox7/vega"
+)
+
+type (
+	json struct {
+		Status string `json:"status"`
+	}
+	yaml struct {
+		Status string `yaml:"status"`
+	}
+	xml struct {
+		Status string `xml:"status"`
+	}
+)
+
+func bind() {
+	router := vega.NewRouter()
+
+	router.Post("/ping", func(ctx *vega.Context) {
+		pong, _ := ctx.BindString()
+		fmt.Println(pong)
+	})
+	router.Post("/json", func(ctx *vega.Context) {
+		var json json
+		ctx.BindJSON(&json)
+	})
+	router.Post("/yaml", func(ctx *vega.Context) {
+		var yaml yaml
+		ctx.BindYAML(&yaml)
+	})
+	router.Post("/xml", func(ctx *vega.Context) {
+		var xml xml
+		ctx.BindXML(xml)
+	})
+
+	router.Run(":8080")
+}
--- examples/example-basic.go
+++ examples/example-basic.go
@@ -2,11 +2,17 @@ package examples
 
 import "github.com/gox7/vega"
 
-func main() {
+type (
+	xml struct {
+		Status string `xml:"status"`
+	}
+)
+
+func basic() {
 	router := vega.NewRouter()
 
 	router.Get("/ping", func(ctx *vega.Context) {
-		ctx.WriteString(200, "Hello, world!")
+		ctx.WriteString(200, "pong")
 	})
 	router.Get("/json", func(ctx *vega.Context) {
 		ctx.JSON(200, vega.H{"status": "ok"})
@@ -15,7 +21,7 @@ func main() {
 		ctx.YAML(200, vega.H{"status": "ok"})
 	})
 	router.Get("/xml", func(ctx *vega.Context) {
-		ctx.XML(200, "status - ok")
+		ctx.XML(200, xml{Status: "ok"})
 	})
 
 	router.Run(":8080")
