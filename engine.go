85456178305e3d9f8ddf2eb21a8177724381cedb
Ilya <maslennikovila82@gmail.com>
2025-07-19 11:07:18 +0300

    Added group

--- engine.go
+++ engine.go
@@ -8,39 +8,43 @@ import (
 
 type (
 	Engine struct {
-		router map[string]func(*Context)
+		handlers map[string]func(*Context)
+		group    Group
 	}
 	H map[string]any
 )
 
 func NewRouter() *Engine {
 	return &Engine{
-		router: make(map[string]func(*Context)),
+		handlers: make(map[string]func(*Context)),
 	}
 }
 
 func (eng *Engine) Get(pattern string, handler func(ctx *Context)) {
-	eng.router["GET||"+pattern] = handler
+	eng.handlers["GET||"+pattern] = handler
 }
 
 func (eng *Engine) Post(pattern string, handler func(ctx *Context)) {
-	eng.router["POST||"+pattern] = handler
+	eng.handlers["POST||"+pattern] = handler
 }
 
 func (eng *Engine) Put(pattern string, handler func(ctx *Context)) {
-	eng.router["PUT||"+pattern] = handler
+	eng.handlers["PUT||"+pattern] = handler
 }
 
 func (eng *Engine) Patch(pattern string, handler func(ctx *Context)) {
-	eng.router["PATCH||"+pattern] = handler
+	eng.handlers["PATCH||"+pattern] = handler
 }
 
 func (eng *Engine) Delete(pattern string, handler func(ctx *Context)) {
-	eng.router["DELETE||"+pattern] = handler
+	eng.handlers["DELETE||"+pattern] = handler
 }
 
 func (eng *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if handler, ok := eng.router[r.Method+"||"+r.URL.Path]; ok {
+	if handler, ok := eng.handlers[r.Method+"||"+r.URL.Path]; ok {
+		context := newContext(w, r)
+		handler(context)
+	} else if handler, ok := eng.group.handlers[r.Method+"||"+r.URL.Path]; ok {
 		context := newContext(w, r)
 		handler(context)
 	} else {
---
+++ group.go
@@ -0,0 +1,57 @@
+package vega
+
+import "strings"
+
+type (
+	Group struct {
+		name     string
+		handlers map[string]func(*Context)
+	}
+)
+
+func (eng *Engine) Group(name string) *Group {
+	if name[:1] != "/" {
+		name = "/" + name
+	}
+	if name[1:] != "/" {
+		name = name + "/"
+	}
+
+	var group Group
+	group.name = name
+	group.handlers = make(map[string]func(*Context))
+	eng.group = group
+	return &group
+}
+
+func checkPattern(pattern *string) {
+	data := *pattern
+	if strings.HasPrefix(data, "/") {
+		*pattern = data[1:]
+	}
+}
+
+func (grp *Group) Get(pattern string, handler func(*Context)) {
+	checkPattern(&pattern)
+	grp.handlers["GET||"+grp.name+pattern] = handler
+}
+
+func (grp *Group) Post(pattern string, handler func(*Context)) {
+	checkPattern(&pattern)
+	grp.handlers["POST||"+grp.name+pattern] = handler
+}
+
+func (grp *Group) Put(pattern string, handler func(*Context)) {
+	checkPattern(&pattern)
+	grp.handlers["PUT||"+grp.name+pattern] = handler
+}
+
+func (grp *Group) Patch(pattern string, handler func(*Context)) {
+	checkPattern(&pattern)
+	grp.handlers["PATCH||"+grp.name+pattern] = handler
+}
+
+func (grp *Group) Delete(pattern string, handler func(*Context)) {
+	checkPattern(&pattern)
+	grp.handlers["DELETE||"+grp.name+pattern] = handler
+}
